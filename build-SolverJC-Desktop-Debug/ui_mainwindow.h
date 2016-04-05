e845a1c29f467b93d8563131125c7e3f666bb5db
Q1minus <q1munys@gmail.com>
2016-04-05 21:20:10 +0300

    Story 3: I try to work in QtCreator

---
+++ build-SolverJC-Desktop-Debug/ui_mainwindow.h
@@ -0,0 +1,119 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QLabel *nLabel;
+    QLineEdit *nLineEdit;
+    QLabel *mLabel;
+    QLineEdit *mLineEdit;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(989, 677);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        formLayoutWidget = new QWidget(centralWidget);
+        formLayoutWidget->setObjectName(QStringLiteral("formLayoutWidget"));
+        formLayoutWidget->setGeometry(QRect(20, 10, 101, 71));
+        formLayout = new QFormLayout(formLayoutWidget);
+        formLayout->setSpacing(6);
+        formLayout->setContentsMargins(11, 11, 11, 11);
+        formLayout->setObjectName(QStringLiteral("formLayout"));
+        formLayout->setContentsMargins(0, 0, 0, 0);
+        nLabel = new QLabel(formLayoutWidget);
+        nLabel->setObjectName(QStringLiteral("nLabel"));
+
+        formLayout->setWidget(0, QFormLayout::LabelRole, nLabel);
+
+        nLineEdit = new QLineEdit(formLayoutWidget);
+        nLineEdit->setObjectName(QStringLiteral("nLineEdit"));
+
+        formLayout->setWidget(0, QFormLayout::FieldRole, nLineEdit);
+
+        mLabel = new QLabel(formLayoutWidget);
+        mLabel->setObjectName(QStringLiteral("mLabel"));
+
+        formLayout->setWidget(1, QFormLayout::LabelRole, mLabel);
+
+        mLineEdit = new QLineEdit(formLayoutWidget);
+        mLineEdit->setObjectName(QStringLiteral("mLineEdit"));
+
+        formLayout->setWidget(1, QFormLayout::FieldRole, mLineEdit);
+
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setEnabled(true);
+        pushButton->setGeometry(QRect(20, 90, 99, 27));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 989, 25));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::LeftToolBarArea, mainToolBar);
+        MainWindow->insertToolBarBreak(mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        nLabel->setText(QApplication::translate("MainWindow", "N = ", 0));
+        nLineEdit->setText(QApplication::translate("MainWindow", "<30", 0));
+        mLabel->setText(QApplication::translate("MainWindow", "M = ", 0));
+        mLineEdit->setText(QApplication::translate("MainWindow", "<30", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Enter", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
