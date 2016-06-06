47a450e7860556a08cc2c52bfd401d8e3b2312e5
Q1minus <q1munys@gmail.com>
2016-06-06 23:23:23 +0300

    Menu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <SFML/Graphics.hpp>
 
+
 using namespace sf;
 
 const int SizeCell = 32;
@@ -65,6 +66,7 @@ void destroy_arr(int **arr, int m)
 	delete [] arr;
 }
 
+
 class FieldCreator{
 public:
 	int m, n, w, h;
@@ -72,25 +74,27 @@ public:
 	int **arr, **topArr, **leftArr;
 	std::string nonName;
 
-	Image menuImage;
+	enum States {MAIN, CREATE, SOLVE, FIELD, CREATOR} state;
+
 	Image fieldImage;
-
-	Texture menuTexture;
 	Texture fieldTexture;
-
-	Sprite menuSprite;
 	Sprite fieldSprite;
 	Sprite matrixSprite;
 
 	Font font;
 	Text text;
 
+	void SetMandN(int M, int N)
+	{
+		m = M; n = N; //m = width, n = heigth cells
+		arr = create_arr(M, N);
+	}
+
 	FieldCreator(int M, int N)
 	{
 		m = M; n = N; //m = width, n = heigth cells
-		w = 5*SizeCell; h = 2*4*SizeCell;
 		topM = topN = 0;
-		leftM = leftN = 0;
+		leftM = leftN = 0;		
 
 		font.loadFromFile("Font.ttf");
 		text.setFont(font);
@@ -109,11 +113,6 @@ public:
 
 		matrixSprite.setTexture(fieldTexture);
 		matrixSprite.setTextureRect(IntRect(96, 0, SizeCell, SizeCell));
-
-		menuImage.loadFromFile("images/menu.jpg");
-		menuTexture.loadFromImage(menuImage);
-		menuSprite.setTexture(menuTexture);
-		menuSprite.setTextureRect(IntRect(0, 0, w, h));
 
 	}
 
@@ -266,8 +265,9 @@ public:
 				leftArr = create_arr(leftM, leftN);
 
 				/*Задаем имя файла, затем полный путь к матрицам*/
-				std::cout << "Crossword name: ";
-				std::cin >> nonName;
+
+				nonName = "Crossword#1";
+				std::cout << nonName << std::endl;
 				std::string topFile = "Data/TopMatrix/" + nonName;
 				std::string leftFile = "Data/LeftMatrix/" + nonName;
 				std::string fieldFile = "Data/" + nonName;
@@ -362,6 +362,7 @@ public:
 				arr = read_arr(nonName, m, n);
 			}
 				break;
+
 			default:
 				break;
 		}
@@ -392,176 +393,405 @@ public:
 		}
 	}
 
+	void DrawField(RenderWindow *window)
+	{
+		/*Рисуем поле с привязкой к массиву*/
+		for (int j = 0; j < n; j++){
+			for (int i = 0; i < m; i++){
+				if (arr[i][j] == 0){//GRAY
+					fieldSprite.setTextureRect(IntRect(0, 0, SizeCell, SizeCell));
+				}
+				if (arr[i][j] == 1){//CROSS
+					fieldSprite.setTextureRect(IntRect(32, 0, SizeCell, SizeCell));
+				}
+				if (arr[i][j] == 2){//BLACK
+					fieldSprite.setTextureRect(IntRect(64, 0, SizeCell, SizeCell));
+				}
+
+				fieldSprite.setPosition( (leftN + i + 5)*SizeCell, (topN + j) * SizeCell);
+				window->draw(fieldSprite);
+				//std::cout << arr[i][j] << " ";
+			}
+			//std::cout  << std::endl;
+		}
+
+	}
+
+	void DrawLeftMatrix(RenderWindow *window)
+	{
+		/*Рисуем левую матрицу*/
+		for (int i = 0; i < leftM; i++) {
+			for (int j = 0; j < leftN; j++) {
+				int X = (j + 5) * SizeCell;
+				int Y =  (i + topN) * SizeCell;
+				matrixSprite.setPosition(X, Y);
+				window->draw(matrixSprite);
+			}
+		}
+
+		for (int i = 0; i < leftM; i++) {
+			int len = length_arr(leftArr[i], leftN);
+
+			for (int j = 0; j < leftN; j++) {
+				int X = (j + 5 + leftN - len) * SizeCell;
+				int Y =  (i + topN) * SizeCell;
+
+				if (leftArr[i][j]) {
+					std::ostringstream matrixItem;
+					matrixItem << leftArr[i][j];
+					text.setPosition(X + 2, Y + 2);
+					text.setString(matrixItem.str());
+					window->draw(text);
+				}
+			}
+		}
+
+	}
+
+	void DrawTopMatrix(RenderWindow *window)
+	{
+		/*Рисуем верхнюю матрицу*/
+		for (int i = 0; i < topM; i++) {
+			for (int j = 0; j < topN; j++) {
+				int X = (i + 5 + leftN) * SizeCell;
+				int Y = j * SizeCell;
+				matrixSprite.setPosition(X, Y);
+				window->draw(matrixSprite);
+
+			}
+		}
+
+		for (int i = 0; i < topM; i++) {
+			int len = length_arr(topArr[i], topM);
+
+			for (int j = 0; j < topN; j++) {
+				int X = (i + 5 + leftN) * SizeCell;
+				int Y = (j + topN - len) * SizeCell;
+
+				if (topArr[i][j]){
+					std::ostringstream matrixItem;
+					matrixItem << topArr[i][j];
+					text.setPosition(X + 2, Y + 2);
+					text.setString(matrixItem.str());
+					window->draw(text);
+				}
+
+			}
+		}
+
+	}
+
+	void Click(RenderWindow *window, Event event, int st)
+	{
+		int xpos = Mouse::getPosition(*window).x / SizeCell;
+		int ypos = Mouse::getPosition(*window).y / SizeCell;
+
+		/*Меняем цвет клеток по клику*/
+		if (m && n && xpos >= 5 + leftN && ypos >= topN) {
+			change_color(event.mouseButton.button, xpos - 5 - leftN, ypos - topN);
+		}
+		else if (xpos < 5) {
+
+			/*OPEN*/
+			if (ypos >= 0 && ypos < 2) {
+				Press(0);
+				int X = (leftN + m + 5)*SizeCell;
+				int Y = (n + topN)*SizeCell;
+				window->create(sf::VideoMode(X, Y), "Nonagram", sf::Style::Close);
+			}
+
+			/*LOAD*/
+			if (ypos > 1 && ypos < 4){
+				Press(1);
+				int X = (leftN + m + 5)*SizeCell;
+				int Y = (n + topN)*SizeCell;
+				window->create(sf::VideoMode(X, Y), "Nonagram", sf::Style::Close);
+			}
+
+			/*CLEAR*/
+			if (ypos > 3 && ypos < 6)
+				Press(2);
+
+			/*SOLVE*/
+			if (st == SOLVE && ypos > 5 && ypos < 8)
+				Press(3);
+
+		}
+
+	}
+
 };
+
+class Menu
+{
+public:
+	int w, h;
+	enum States {MAIN, CREATE, SOLVE, FIELD, CREATOR} state;
+
+	Image image;
+	Texture texture;
+	Sprite sprite;
+
+	Font font;
+	Text text;
+
+	Menu()
+	{
+		w = 15 * SizeCell; h = 6 * 3 * SizeCell;
+		state = MAIN;
+
+		image.loadFromFile("images/MenuItem.jpg");
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(0, 0, w, h));
+
+		font.loadFromFile("Font.ttf");
+		text.setFont(font);
+		text.setCharacterSize(70);
+		text.setColor(Color::White);
+	}
+
+	void MenuDraw(RenderWindow *window)
+	{
+		switch (state) {
+			case MAIN:
+				MainMenu(window);
+				break;
+			case CREATE:
+				InputMenu(window);
+				break;
+			case SOLVE:
+				break;
+			case FIELD:
+				break;
+			case CREATOR:
+				MenuCreator(window);
+				break;
+		}
+
+	}
+
+	void MenuTemplate(RenderWindow *window, int n, int x, int y, std::string titles[])
+	{
+		x *= SizeCell, y *= SizeCell; // Положение спрайта
+		sprite.setScale(1, 1);
+
+		for (int i = 0; i < n; i++) {
+			sprite.setPosition(x, y);
+			text.setString(titles[i]);
+			text.setPosition( x + 1.4 * SizeCell, y + 2.5 * SizeCell);
+			y += 6 * SizeCell;
+
+			window->draw(sprite);
+			window->draw(text);
+		}
+
+	}
+
+	void Click(RenderWindow *window, FieldCreator *cr, Event event)
+	{
+		int xpos = Mouse::getPosition(*window).x / SizeCell;
+		int ypos = Mouse::getPosition(*window).y / SizeCell;
+
+		switch (state) {
+			case MAIN:
+			{
+					if (ypos >= 0 && ypos < 6)
+						Press(0, window);
+					if (ypos > 6 && ypos < 12)
+						Press(1, window);
+					if (ypos > 12 && ypos < 18)
+						Press(2, window);
+			}
+				break;
+			case CREATE:
+			{
+				if (xpos >= 0 && xpos < 15)
+				{
+					if (ypos >= 0 && ypos < 6)
+						cr->SetMandN(10, 10);
+					if (ypos > 6 && ypos < 12)
+						cr->SetMandN(10, 15);
+					if (ypos > 12 && ypos < 18)
+						cr->SetMandN(10, 20);
+					if (ypos > 18 && ypos < 24)
+						cr->SetMandN(15, 30);
+				}
+
+				if (xpos > 15 && xpos < 30)
+				{
+					if (ypos >= 0 && ypos < 6)
+						cr->SetMandN(20, 20);
+					if (ypos > 6 && ypos < 12)
+						cr->SetMandN(20, 25);
+					if (ypos > 12 && ypos < 18)
+						cr->SetMandN(20, 30);
+					if (ypos > 18 && ypos < 24)
+						cr->SetMandN(25, 20);
+				}
+
+				if (xpos > 30 && xpos < 45)
+				{
+					if (ypos >= 0 && ypos < 6)
+						cr->SetMandN(25, 25);
+					if (ypos > 6 && ypos < 12)
+						cr->SetMandN(25, 30);
+					if (ypos > 12 && ypos < 30)
+						cr->SetMandN(30, 30);
+					if (ypos > 18 && ypos < 15)
+						cr->SetMandN(30, 20);
+				}
+
+				state = CREATOR;
+				window->create(sf::VideoMode ( (cr->m + 5) * SizeCell, cr->n * SizeCell), "Creator", sf::Style::Close);
+			}
+				break;
+			case SOLVE:
+				break;
+			case FIELD:
+				break;
+			case CREATOR:
+			{
+				cr->Click(window, event, state);
+			}
+				break;
+		}
+
+	}
+
+	void MenuCreator(RenderWindow *window)
+	{
+		int x = 0 * SizeCell;
+		int y = 0 * SizeCell;
+		int n = 3;
+
+		std::string titles[] = {"Load", "Save", "Clear"}; // Заголовки
+		text.setCharacterSize(24);
+		sprite.setScale((float)1/3, (float)1/3);
+
+		for (int i = 0; i < n; i++) {
+			sprite.setPosition(x * SizeCell, y * SizeCell);
+			text.setString(titles[i]);
+			text.setPosition( (x + 1) * SizeCell, (y + 0.75) * SizeCell);
+			y += 2;
+
+			window->draw(sprite);
+			window->draw(text);
+		}
+
+	}
+
+	void MenuField(RenderWindow *window)
+	{
+		std::string titles[] = {"Check", "Hint", "Clear", "Solve", "Save"}; // Заголовки
+		MenuTemplate(window, 5, 0, 0, titles);
+	}
+
+	void MainMenu(RenderWindow *window)
+	{
+		std::string titles[] = {"Solve", "Create", "Exit"}; // Заголовки
+		MenuTemplate(window, 3, 0, 0, titles);
+	}
+
+	void InputMenu(RenderWindow *window)
+	{
+		int x = 0 * SizeCell, y = 0 * SizeCell; // Положение спрайта
+		int n = 12; // Количество кнопок
+
+		std::string titles[] = {"10x10", "10x15", "10x20",
+														"15x30", "20x20", "20x25",
+														"20x30", "25x20", "25x25",
+														"25x30", "30x15", "30x20"};
+
+		text.setCharacterSize(40);
+		sprite.setScale(1,1);
+
+		for (int i = 0; i < n; i++) {
+			sprite.setPosition(x, y);
+			text.setString(titles[i]);
+			text.setPosition( x + 2.5 * SizeCell, y + 2.5 * SizeCell);
+			y += 6 * SizeCell;
+
+			if (i % 4 == 3) {
+				y = 0 * SizeCell;
+				x += 15 * SizeCell;
+			}
+
+
+			window->draw(sprite);
+			window->draw(text);
+		}
+
+	}
+
+	void Press(int button, RenderWindow *window)
+	{
+		switch (button) {
+			/*SOLVE*/
+			case 0:
+			{
+				std::cout << "1" << std::endl;
+			}
+				break;
+
+			/*CREATE*/
+			case 1:
+			{
+				w = 45 * SizeCell, h = 24 * SizeCell;
+				window->create(sf::VideoMode(w, h), "Create", sf::Style::Close);
+				state = CREATE;
+			}
+				break;
+
+			/*EXIT*/
+			case 2:
+			{
+				window->close();
+			}
+				break;
+
+			default:
+				break;
+		}
+	}
+
+
+};
+
 
 int main()
 {
-	/*Спрашиваем, хотят ли создать свой нонаграм.
-	Да - Задаем параметры поля m и n,
-	Нет - Появляется меню и поле 8х8(минисальное)*/
-	std::string m_str;
-	std::string n_str;
 	int m = 0, n = 0;
-	std::string answer;
-
-	std::cout << "You want to create your nonagram? (y/n): ";
-	std::cin >> answer;
-
- 	if (answer == "y" || answer == "yes") {
-			std::cout << "-> Min size 8x8" << std::endl;
-			std::cout << "-> Max size 100x100" << std::endl;
-			std::cout << "-> Horizontal length m = ";
-			std::cin >> m_str;
-			m = strToInt(m_str);
-
-			std::cout << "-> Vertical length n = ";
-			std::cin >> n_str;
-			n = strToInt(n_str);
-
-			if (m < 8 || n < 8){
-				std::cout << "-> Set min size 8x8" << std::endl;
-				m = 8;
-				n = 8;
-			}
-			if(m > 100 && n > 100){
-				std::cout << "-> Set max size 100x100" << std::endl;
-				m = 100;
-				n = 100;
-			}
-
-	}
-	else {
-		std::cout << "Set min size 8x8\n" << std::endl;
-		m = 8;
-		n = 8;
-	}
-
-	/*Создаем поле*/
 	FieldCreator creator(m, n);
 
-	RenderWindow window(sf::VideoMode((creator.m + 5)*SizeCell, creator.n*SizeCell), "Creator JC", sf::Style::Close);
+	/*Создаем меню*/
+	Menu menu;
+	RenderWindow window(sf::VideoMode(menu.w, menu.h), "Main Menu", sf::Style::Close);
+	window.setPosition(Vector2i(600, 237));
 
 	while (window.isOpen())
 	{
 		Event event;
 
-		while (window.pollEvent(event)){
+		while (window.pollEvent(event)) {
 			if(event.type == sf::Event::Closed)
 				window.close();
 
-      /* "Оживляем" меню*/
-			if(event.type == Event::MouseButtonPressed){
-				int xpos = Mouse::getPosition(window).x / SizeCell;
-				int ypos = Mouse::getPosition(window).y / SizeCell;
-
-				/*Меняем цвет клеток по клику*/
-				if (xpos >= 5 + creator.leftN && ypos >= creator.topN){
-					creator.change_color(event.mouseButton.button, xpos - 5 - creator.leftN, ypos - creator.topN);
-				}
-				else if (xpos < 5){ //menu item
-					if(ypos >= 0 && ypos < 2){//OPEN
-						creator.Press(0);
-						int X = (creator.leftN + creator.m + 5)*SizeCell;
-						int Y = (creator.n + creator.topN)*SizeCell;
-						window.create(sf::VideoMode(X, Y), "JCrossword", sf::Style::Close);
-
-					}
-					if (ypos > 1 && ypos < 4){//LOAD
-						creator.Press(1);
-						int X = (creator.leftN + creator.m + 5)*SizeCell;
-						int Y = (creator.n + creator.topN)*SizeCell;
-						window.create(sf::VideoMode(X, Y), "JCrossword", sf::Style::Close);
-
-					}
-					if (ypos > 3 && ypos < 6)//CLEAR
-						creator.Press(2);
-					if (ypos > 5 && ypos < 8)//SOLVE
-						creator.Press(3);
-
-				}
-
+			if(event.type == Event::MouseButtonPressed) {
+				menu.Click(&window, &creator, event);
 			}
-		}
+
+
+		}//while event
 
 		window.clear();
 
-		/*Рисуем верхнюю матрицу*/
-		for (int i = 0; i < creator.topM; i++) {
-			for (int j = 0; j < creator.topN; j++) {
-				int X = (i + 5 + creator.leftN) * SizeCell;
-				int Y = j * SizeCell;
-				creator.matrixSprite.setPosition(X, Y);
-				window.draw(creator.matrixSprite);
+		menu.MenuDraw(&window);
 
-			}
-		}
+		creator.DrawTopMatrix(&window);
+		creator.DrawLeftMatrix(&window);
+		creator.DrawField(&window);
 
-		for (int i = 0; i < creator.topM; i++) {
-			int len = length_arr(creator.topArr[i], creator.topM);
-
-			for (int j = 0; j < creator.topN; j++) {
-				int X = (i + 5 + creator.leftN) * SizeCell;
-				int Y = (j + creator.topN - len) * SizeCell;
-
-				if (creator.topArr[i][j]){
-					std::ostringstream matrixItem;
-					matrixItem << creator.topArr[i][j];
-					creator.text.setPosition(X + 2, Y + 2);
-					creator.text.setString(matrixItem.str());
-					window.draw(creator.text);
-				}
-
-			}
-		}
-		/*Рисуем левую матрицу*/
-		for (int i = 0; i < creator.leftM; i++) {
-			for (int j = 0; j < creator.leftN; j++) {
-				int X = (j + 5) * SizeCell;
-				int Y =  (i + creator.topN) * SizeCell;
-				creator.matrixSprite.setPosition(X, Y);
-				window.draw(creator.matrixSprite);
-			}
-		}
-
-		for (int i = 0; i < creator.leftM; i++) {
-			int len = length_arr(creator.leftArr[i], creator.leftN);
-
-			for (int j = 0; j < creator.leftN; j++) {
-				int X = (j + 5 + creator.leftN - len) * SizeCell;
-				int Y =  (i + creator.topN) * SizeCell;
-
-				if (creator.leftArr[i][j]){
-					std::ostringstream matrixItem;
-					matrixItem << creator.leftArr[i][j];
-					creator.text.setPosition(X + 2, Y + 2);
-					creator.text.setString(matrixItem.str());
-					window.draw(creator.text);
-				}
-			}
-		}
-
-		/*Рисуем поле с привязкой к массиву*/
-		for (int j = 0; j < creator.n; j++){
-			for (int i = 0; i < creator.m; i++){
-				if (creator.arr[i][j] == 0){//GRAY
-					creator.fieldSprite.setTextureRect(IntRect(0, 0, SizeCell, SizeCell));
-				}
-				if (creator.arr[i][j] == 1){//CROSS
-					creator.fieldSprite.setTextureRect(IntRect(32, 0, SizeCell, SizeCell));
-				}
-				if (creator.arr[i][j] == 2){//BLACK
-					creator.fieldSprite.setTextureRect(IntRect(64, 0, SizeCell, SizeCell));
-				}
-
-				creator.fieldSprite.setPosition( (creator.leftN + i + 5)*SizeCell, (creator.topN + j) * SizeCell);
-				window.draw(creator.fieldSprite);
-				//std::cout << creator.arr[i][j] << " ";
-			}
-			//std::cout  << std::endl;
-		}
-
-		/*Рисуем меню*/
-		window.draw(creator.menuSprite);
 		window.display();
 	}
 	return 0;
